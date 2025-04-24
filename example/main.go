e5580f6362a9fc07035ff0372da64b7a23e4a4c9
Ruslan Kosykh <rusbko@gmail.com>
2025-04-24 12:07:28 +0300

    hotfix go.mod

--- example/main.go
+++ example/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/flowchartsman/swaggerui"
+	"github.com/ruko1202/swaggerui"
 )
 
 //go:embed spec/petstore.yml
@@ -16,6 +16,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/pet/", petHandler)
 	mux.Handle("/swagger/", http.StripPrefix("/swagger", swaggerui.Handler(spec)))
-	log.Println("serving on :8080")
+	log.Println("serving on http://localhost:8080")
+	log.Println("swagger on http://localhost:8080/swagger")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
